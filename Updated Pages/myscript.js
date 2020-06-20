d60b3a65d98bd799c8f721e69e1a52e8bc14119b
cbrizell <59903787+cbrizell@users.noreply.github.com>
2020-06-19 21:25:22 -0400

    Add files via upload
    Creation of edit page, js to create highlightable cells and CSS to apply those changes.
    Directions for using the site is displayed at the top.

---
+++ Updated Pages/myscript.js
@@ -0,0 +1,44 @@
+// Wait until the DOM is loaded
+window.addEventListener("DOMContentLoaded", function(){
+
+  // Get all the td elements in an array
+  var theCells = document.getElementsByTagName("td");
+  
+  // Loop through each td
+  for(var i = 0; i < theCells.length; ++i){
+    
+     // Set up a click event handler for the td
+     theCells[i].addEventListener("click", function(){
+       
+       // Check to see if the td has the class attribute applied already
+       if(this.getAttribute("class") === "highlight"){
+         // If so, remove it
+         this.removeAttribute("class");
+       } else {
+         // If not, add it
+         this.setAttribute("class", "highlight");     
+       }
+     });
+  }
+  
+});
+function myFunction() {
+  var txt;
+  var r = confirm("Are you sure you want to save?");
+  if (r == true) {
+    txt = window.location.href='Calendar_page.html';
+  } else {
+    txt = "You pressed Cancel!";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
+function myFunction2() {
+  var txt;
+  var r = confirm("Are you sure you want to leave without saving?");
+  if (r == true) {
+    txt = window.location.href='Calendar_page.html';
+  } else {
+    txt = "You pressed Cancel!";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
